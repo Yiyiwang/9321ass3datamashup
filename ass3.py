4db3c1e4c69185b48c8be1f432f4cfd4637de48b
joseph <jt@mimic.localdomain>
2018-05-18 18:34:02 +1000

    for some reason git did not update file, read commit b27517f2703f0923c2f52839b8d7abdd89af559e for details on this push

--- ass3.py
+++ ass3.py
@@ -1,23 +1,22 @@
-from flask import Flask
+from flask import jsonify, Flask
 from flask_restful import reqparse
-from flask import jsonify
+from os.path import dirname, isfile, realpath
+from xlrd import open_workbook, sheet
 import csv
-import os,xlrd
 
 app = Flask(__name__)
-path=os.path.dirname(__file__)
-country_code={}
-
+resdir = dirname(realpath(__file__)) + "/resources/"
+country_code = {}
 
 def read_file():
-    workbook = xlrd.open_workbook(path+"/Country-Code.xlsx")
+    workbook = xlrd.open_workbook(resdir + "/Country-Code.xlsx")
+    if len(workbook.sheet_names()) < 1:
+        return
     sheet1 = workbook.sheets()[0]
     num_rows = sheet1.nrows
     for i in range(1,num_rows):
         row = sheet1.row(i)
-        #print(int(row[0].value),row[1].value)
         country_code[row[1].value] = int(row[0].value)
-   # print(country_code)
 
   #  with open(path+"/tripadvisor_in-restaurant_sample.csv") as f:
    #     f_csv = csv.DictReader(f)
@@ -31,34 +30,25 @@ def get_data_by_location():
     parser.add_argument('location', type=str)
     args = parser.parse_args()
     location = args.get("location")
-    #print(location)
     if location in country_code:
         loca_code = country_code[location]
-        #print(loca_code)
+
+    with open(resdir + "/tripadvisor_in-restaurant_sample.csv") as f:
+        f_csv = csv.DictReader(f)
+        for row in f_csv :
+            if row['Country'] == location:
+                print(row)
+
     if loca_code is None:
-         with open(path+"/tripadvisor_in-restaurant_sample.csv") as f:
-             f_csv = csv.DictReader(f)
-             for row in f_csv :
-                 if row['Country'] == location:
-                     print(row)
-    else:
-         with open(path+"/tripadvisor_in-restaurant_sample.csv") as f, open(path+"/zomato.csv") as file:
-             f_csv = csv.DictReader(f)
-             for row in f_csv :
-                 #print(row['Country'])
-                 #print(row)
-                 if row['Country'] == location:
-                    print(row)
-             fi_csv = csv.DictReader(file)
-             for each_row in fi_csv:
-                 if each_row['Country Code'] == loca_code:
-                     print(each_row)
+        return jsonify("ok"), 200
 
-         return jsonify("ok"), 200
+    with open(resdir + "/zomato.csv") as f1:
+         fi_csv = csv.DictReader(f1)
+         for each_row in fi_csv:
+             if each_row['Country Code'] == loca_code:
+                 print(each_row)
 
-
-
-        
+    return jsonify("ok"), 200
 
 if __name__ == '__main__':
     read_file()
