e82bf2e50dbf52e080b5942cc0d4a727532b38a8
joseph <jt@mimic.localdomain>
2018-05-19 15:32:58 +1000

    added zomato restaurant lookups by given city, not tested yet, added zomato api key

--- ass3.py
+++ ass3.py
@@ -1,11 +1,13 @@
+from collection import OrderedDict
 from flask import jsonify, Flask
 from flask_restful import reqparse
+from json import loads, dumps
 from os import makedirs, remove
 from os.path import dirname, exists, isfile, realpath
 from pathlib import Path
+from requests import get, post
 from shutil import copyfile, rmtree
 from subprocess import call
-from requests import get, post
 from xlrd import open_workbook, sheet
 from zipfile import ZipFile
 import csv
@@ -16,6 +18,8 @@ resdir = dirname(realpath(__file__)) + "/resources/"
 countrycode_fname = "Country-Code.xlsx"
 tripadvisor_fname = "tripadvisor_in-restaurant_sample.csv"
 zomato_fname = "zomato.csv"
+
+zomato_api_baseurl = "https://developers.zomato.com/api/v2.1/"
 
 country_code = {}
 
@@ -66,6 +70,77 @@ def read_file():
     for i in range(1,num_rows):
         row = sheet1.row(i)
         country_code[row[1].value] = int(row[0].value)
+
+def get_zomato_key():
+    if not isfile(resdir + "zomato.json"):
+        return ""
+
+    with open(resdir + "zomato.json", "r") as f:
+        data = loads(f.read().replace("\n", "").strip().lower())
+
+    return data['user-key']
+
+@app.route("/restaurants/<string:city>", methods=['Get'])
+def get_zomato_rest_by_loc(city):
+    if not city:
+        return dumps({"message" : "empty city name"}), 400
+
+    city = city.strip().lower()
+    start = 0
+    if 'start' in request.args:
+        try:
+            start = int(request.args.get('start'))
+        except ValueError:
+            pass # do nothing, assume default value
+    count = 20
+    if 'count' in request.args:
+        try:
+            count = int(request.args.get('count'))
+        except ValueError:
+            pass # do nothing, assume default value
+    pages = 1
+    if 'pages' in request.args:
+        try:
+            pages = int(request.args.get('pages'))
+        except ValueError:
+            pass # do nothing, assume default value
+    headers = {
+        "Content-Type" : "application/json",
+        "user-key" : get_zomato_key()
+    }
+    url = zomato_api_baseurl + "cities?q=" + city
+    req = get(url, headers)
+    res = loads(req.content)
+
+    if len(res["location_suggestions"]) < 1:
+        return dumps({"message" : "city not found"}), 404
+
+    d = OrderedDict()
+    d["results_found"] = 0
+    d["restaurants"] = []
+    for sug in res["location_suggestions"]:
+        sug_id = sug["id"]
+        sug_name = sug["name"]
+        url = zomato_api_baseurl + "locations?query=" + sug_name
+        req = get(url, headers)
+        res1 = loads(req.content)
+
+        if len(res1["location_suggestions"]) < 1:
+            continue
+
+        entity_id = res1["location_suggestions"][0]["entity_id"]
+        entity_type = res1["location_suggestions"][0]["entity_type"]
+
+        for i in range(pages):
+            url1 = zomato_api_baseurl + "search?entity_id=" + entity_id +\
+                    "&entity_type=" + entity_type +\
+                    "&start=" + str(start + i * count) + "&count=" + str(count)
+            req1 = get(url1, headers)
+            res2 = loads(req1.content)
+
+            d["results_found"] += len(res2["restaurants"])
+            for rest in res2["restaurants"]:
+                d.append(rest)
 
 @app.route("/api/location", methods=['Get'])
 def get_data_by_location():
